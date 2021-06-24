9686750835058f525a051bfcad066debc22b1d97
SpiderBeaver <spdrbeaver@gmail.com>
2021-06-24 16:15:46 +0300

    Products pictures uploading.

--- src/components/NewProductForm.tsx
+++ src/components/NewProductForm.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
+import { ProductDto } from '../dto/ProductDto';
 
 export default function NewProductForm() {
   const [name, setName] = useState('');
+  const [file, setFile] = useState<File | null>(null);
 
   const handleSubmit: React.FormEventHandler = async (e) => {
     e.preventDefault();
@@ -10,7 +12,7 @@ export default function NewProductForm() {
       return;
     }
 
-    const result = await fetch('http://localhost:3001/products', {
+    const response = await fetch('http://localhost:3001/products', {
       method: 'POST',
       body: JSON.stringify({
         product: {
@@ -22,16 +24,25 @@ export default function NewProductForm() {
       },
       mode: 'cors',
     });
+    const newProduct = (await response.json()) as ProductDto;
 
-    if (result.ok) {
-      setName('');
-    }
+    const formData = new FormData();
+    formData.append('file', file as Blob);
+
+    const uploadUrl = `http://localhost:3001/products/${newProduct.id}/picture`;
+    const uploadResponse = await fetch(uploadUrl, {
+      method: 'POST',
+      body: formData,
+    });
+    const uploadResponseData = await uploadResponse.json();
+    console.log(uploadResponseData);
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <label>Name</label>
       <input type="text" value={name} onChange={(e) => setName(e.target.value)}></input>
+      <input type="file" name="picture" onChange={(e) => setFile(e.target.files?.[0] || null)}></input>
       <input type="submit" value="New Product"></input>
     </form>
   );
